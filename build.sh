5ba6e3c9cbd377bdc041dc67d1146451bccd0c91
Maxim Bazarov <maxim.bazarov@ingrammicro.com>
2020-04-08 19:09:54 +0300

    Added CB changes

---
+++ build.sh
@@ -0,0 +1,4 @@
+cp -rf /Users/baza/Ingram/Git/ingram-micro-cloud/cb-modern-ui/images/* images/
+cp -f /Users/baza/Ingram/Git/uxf.sketchlibrary/cb-modern-ui/cb-modern-ui-elements-tokens.scss src/common/
+cp -f /Users/baza/Ingram/Git/uxf.sketchlibrary/cb-modern-ui/cb-modern-ui-vars.scss src/common/
+yarn build
