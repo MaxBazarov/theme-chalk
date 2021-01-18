4c4cc7f8ed67e221c0aa4d422236892aa3d8fd89
Maxim Bazarov <maxim.bazarov@ingrammicro.com>
2021-01-18 13:45:59 +0300

    Updated style

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cp -rf /Users/baza/Ingram/Git/ingram-micro-cloud/cb-modern-ui/images/* images/
+cp -rf /Users/baza/Ingram/Git/cb-modern-ui/images/* images/
 cp -f /Users/baza/Ingram/Git/uxf.sketchlibrary/cb-modern-ui/cb-modern-ui-elements-tokens.scss src/common/
 cp -f /Users/baza/Ingram/Git/uxf.sketchlibrary/cb-modern-ui/_pt-assets/cb-modern-ui/vars.scss src/common/cb-modern-ui-vars.scss
 yarn build
