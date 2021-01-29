21ed1a4c4f4506bf24e35125647c74d18fa30ecc
Maxim Bazarov <maxim.bazarov@ingrammicro.com>
2021-01-29 13:32:34 +0300

    Improved alerts

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ cp -rf /Users/baza/Ingram/Git/cb-modern-ui/images/* images/
 cp -f /Users/baza/Ingram/Git/uxf.sketchlibrary/cb-modern-ui/cb-modern-ui-elements-tokens.scss src/common/
 cp -f /Users/baza/Ingram/Git/uxf.sketchlibrary/cb-modern-ui/_pt-assets/cb-modern-ui/vars.scss src/common/cb-modern-ui-vars.scss
 yarn build
+cp -rf lib/* ../storybook-elementui-mdx/node_modules/element-ui/lib/theme-chalk/
